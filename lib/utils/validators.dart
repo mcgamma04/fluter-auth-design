d4f93731c7057e1777fedf903ff8d57b3c9b9a66
Adebayo Michael <mcgamma04@yahoo.com>
2024-06-03 05:57:04 +0100

    added validation util

---
+++ lib/utils/validators.dart
@@ -0,0 +1,22 @@
+class Validators {
+  static String? validateEmail(String value) {
+    if (value.isEmpty) {
+      return 'Email cannot be empty';
+    }
+    final emailRegex = RegExp(r'^[^@]+@[^@]+\.[^@]+');
+    if (!emailRegex.hasMatch(value)) {
+      return 'Enter a valid email';
+    }
+    return null;
+  }
+
+  static String? validatePassword(String value) {
+    if (value.isEmpty) {
+      return 'Password cannot be empty';
+    }
+    if (value.length < 6) {
+      return 'Password must be at least 6 characters';
+    }
+    return null;
+  }
+}
